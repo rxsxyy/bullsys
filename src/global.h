321da2a8098b69a4ba14beb3a42c5cb489d3895c
rxsxyy <rxxxy@protonmail.com>
2026-07-06 08:51:39 -0400

    updated main file and added global variables

---
+++ src/global.h
@@ -0,0 +1,19 @@
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+// Void
+typedef void u0;
+
+// Signed
+typedef int32_t i32;
+
+// Float
+typedef float f32;
+
+// Object size
+typedef size_t usize;
+
+// Pointer size
+typedef ptrdiff_t isize;
--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 
-#include <ini.h>
+#include "global.h"
 #include <stdio.h>
 
-int main(void) {
-    printf("bullsys\n");
+static void usage(const char *prog) {
+    printf("Usage: %s <options> <profile> <action>\n", prog);
+    printf("\nOptions:\n");
+    printf("  -f FILE   Specify .bull file (default: build.bull)\n");
+    printf("  -v        Verbose output\n");
+    printf("  -n        Dry run (print commands without executing)\n");
+    printf("  --help    Show this help\n");
+    printf("  --version Shows executable version\n");
+}
+
+i32 main(i32 argc, char *argv[]) {
+
+    usage(argv[0]);
+
+    return 0;
 }
